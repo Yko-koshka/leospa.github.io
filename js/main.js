5f4b4bff0fc842050f3c50e5a0ab0f81d72d3616
Yko-koshka <57110309+Yko-koshka@users.noreply.github.com>
2020-08-12 10:29:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Hamburger
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu"),
+    menuItems = document.querySelectorAll(".header__nav-link"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+    });
+  });
+});
+
+// Slider
+
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll(".testimonials-inner"),
+  prev = document.getElementById("prev"),
+  next = document.getElementById("next"),
+  dotsWrap = document.querySelector(".slider-dots"),
+  dots = document.querySelectorAll(".dot");
+
+function showSlides(n) {
+  if (n > slides.length) {
+    currentSlideIndex = 0;
+  }
+  if (n < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+
+  slides.forEach((item) => (item.style.display = "none"));
+
+  dots.forEach((item) => item.classList.remove("dot-active"));
+
+  slides[currentSlideIndex].style.display = "block";
+  dots[currentSlideIndex].classList.add("dot-active");
+}
+
+showSlides(currentSlideIndex);
+
+prev.addEventListener("click", function () {
+  currentSlideIndex--;
+  showSlides(currentSlideIndex);
+});
+
+next.addEventListener("click", function () {
+  currentSlideIndex++;
+  showSlides(currentSlideIndex);
+});
+
+dotsWrap.addEventListener("click", function (event) {
+  for (let i = 0; i < dots.length; i++) {
+    if (event.target.classList.contains("dot") && event.target == dots[i]) {
+      currentSlideIndex = i;
+      showSlides(currentSlideIndex);
+    }
+  }
+});
+
+new WOW().init();
